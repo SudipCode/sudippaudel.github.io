7a8f6a635c94a15e2e71b55f5d6739cb4940bddf
memory <91591906+SudipCode@users.noreply.github.com>
2026-07-26 13:50:16 +0545

    fix
    fix

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,9 @@
 function loader(){
     document.querySelector('.loader-container').classList.add('fade-out');
+}
 
+function fadeOut(){
+    setTimeout(loader,3000);
 }
-function fadeoOut(){
-    setInterval(loader,3000);
-}
-window.onload = fadeOut;
+
+window.addEventListener('load', fadeOut);
--- js/tex.js
+++ js/tex.js
@@ -21,23 +21,15 @@ VanillaTilt.init(document.querySelectorAll('.tilt'),{
     max:30
 });
 
-window.onload = () =>{
+window.addEventListener('load', () => {
+    const header2 = document.querySelector('.header .header-2');
 
-    if(window.scrollY > 80){
-      document.querySelector('.header .header-2').classList.add('active');
-    }else{
-      document.querySelector('.header .header-2').classList.remove('active');
+    if (header2) {
+      if (window.scrollY > 80) {
+        header2.classList.add('active');
+      } else {
+        header2.classList.remove('active');
+      }
     }
-  
-    fadeOut();
-  
-  }
-  
-function loader(){
-    document.querySelector('.loader-container').classList.add('active');
-  }
-  
-  function fadeOut(){
-    setTimeout(loader, 4000);
-  }
+});
   
