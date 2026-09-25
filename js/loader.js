953c41dfc72984bd8714151fdc12eb772865fac4
memory <91591906+SudipCode@users.noreply.github.com>
2026-09-25 18:03:54 +0545

    end

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-    setTimeout(loader,3000);
-}
-
-window.addEventListener('load', fadeOut);
